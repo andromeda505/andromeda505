5ea63231faa8b39b38a47a7996fb462d1b9d44a1
andromeda505 <emakbakia369@gmail.com>
2023-08-06 15:55:33 -0600

    change on developbranch

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      Mayte Paola Falcon Acosta. The data scientist you will want to have!
+     This changeis on develop branch. This shouldn't be able on public website
     </main>
   )
 }
