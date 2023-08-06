6d2bb4781b8fafdb89b2a71e0966b2b670d90427
andromeda505 <emakbakia369@gmail.com>
2023-08-06 16:02:35 -0600

    test change on main2

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     Mayte Paola Falcon Acosta. The data scientist you will want to have!qkwdnkqwdnpqwds
+     Mayte Paola Falcon Acosta. The data scientist you will want to have...
     </main>
   )
 }
