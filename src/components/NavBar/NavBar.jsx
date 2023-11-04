9ced59c614433d97329500bc0ae24db1bbf6073b
andromeda505 <emakbakia369@gmail.com>
2023-11-03 21:24:09 -0600

    add effects active link navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,15 +4,18 @@ import { useState } from 'react'
 import './Navbar.css'
 
 export const NavBar = () =>  {
-
+    const [activeLink, setActiveLink] = useState('home')
     const [ menu, setMenu ] = useState(false)
     const toggleMenu = () => {
         setMenu( !menu )
     }
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
 
     return (
         <header className='Cabecera'>
-            <h1 className="Cabecera-h1" onClick={ menu ? toggleMenu : '' }>
+            <h1 className={activeLink === 'home' ? 'Cabecera-h1-act-link' : 'Cabecera-h1'} onClick={ () => {onUpdateActiveLink('home');  menu ? toggleMenu : '' }}>
                 <a href="#home" className="Cabecera-a">Andrómeda</a>
             </h1>
 
@@ -24,9 +27,9 @@ export const NavBar = () =>  {
 
             <nav className={ `Cabecera-nav ${ menu ? 'isActive' : '' }` } >
                 <ul className="Cabecera-ul">
-                    <li className="Cabecera-li"  onClick={ toggleMenu }><a href="#skills" className="Cabecera-a">Skills</a></li>
-                    <li className="Cabecera-li"  onClick={ toggleMenu }><a href="#projects" className="Cabecera-a">Projectos</a></li>
-                    <li className="Cabecera-li"  onClick={ toggleMenu }><a href="#contact" className="Cabecera-a">Contáctame</a></li>
+                    <li className={activeLink === 'skills' && !menu ? 'Cabecera-li-act-link' : 'Cabecera-li'}  onClick={ () => { menu? toggleMenu() : ''; onUpdateActiveLink('skills')} }><a href="#skills" className="Cabecera-a">Skills</a></li>
+                    <li className={activeLink === 'projects' && !menu ? 'Cabecera-li-act-link' : 'Cabecera-li'}  onClick={ () => {menu? toggleMenu() : ''; onUpdateActiveLink('projects')} }><a href="#projects" className="Cabecera-a">Projectos</a></li>
+                    <li className={activeLink === 'contact' && !menu ? 'Cabecera-li-act-link' : 'Cabecera-li'}  onClick={ () => {menu? toggleMenu() : ''; onUpdateActiveLink('contact')} }><a href="#contact" className="Cabecera-a">Contáctame</a></li>
                 </ul>
             </nav>
         </header> 
