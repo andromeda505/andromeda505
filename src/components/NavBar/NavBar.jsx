e2ae46022196f74a72d92b482a0176c68768c204
andromeda505 <emakbakia369@gmail.com>
2023-11-03 22:07:16 -0600

    add scrolled function

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './Navbar.css'
 
 export const NavBar = () =>  {
+    const [scrolled, setScrolled] = useState(false)
     const [activeLink, setActiveLink] = useState('home')
     const [ menu, setMenu ] = useState(false)
     const toggleMenu = () => {
@@ -13,8 +14,20 @@ export const NavBar = () =>  {
         setActiveLink(value);
     }
 
+    useEffect(()=>{
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        }
+        window.addEventListener('scroll', onScroll);
+        return () => window.removeEventListener('scroll', onScroll)
+    },[])
+
     return (
-        <header className='Cabecera'>
+        <header  className={scrolled ? 'Cabecera-scrolled' : 'Cabecera'}>
             <h1 className={activeLink === 'home' ? 'Cabecera-h1-act-link' : 'Cabecera-h1'} onClick={ () => {onUpdateActiveLink('home');  menu ? toggleMenu : '' }}>
                 <a href="#home" className="Cabecera-a">Andrómeda</a>
             </h1>
